5beaa0abaa4660bb702af3d7badc5f1c1f4a915f
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-26 18:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function createDom(root) {
 
+	if(typeof root === "string"){
+		return document.createTextNode(root);
+	}
+
+	const elemt = document.createElement(root.type);
+
+	if(root.attributes){
+		for(const[key,value] of Object.entries(root.attributes)){
+
+			elemt.setAttribute(key,value);
+		}
+	}
+
+	if(root.children){
+		for(cont child of root.children){
+			elemnt.appendChild(createDom(child))
+		}
+	}
+
+	return elemt;
+
 }
 
 module.exports=createDom
